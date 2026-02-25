60d7724c8a04007f290935a0beddb81e4d455dcd
VISHESH SACHAN <visheshsachan021@gmail.com>
2026-02-25 18:42:12 +0530

    refactor: extract shared constants and types

---
+++ typer-app/src/constants.ts
@@ -0,0 +1,9 @@
+export const GITHUB_URL = "https://github.com/vishesh-sachan/typer";
+export const APP_VERSION = "0.1.0";
+
+export type Page = "home" | "settings";
+
+export type Status = {
+  type: "success" | "error" | "info" | "";
+  message: string;
+};
